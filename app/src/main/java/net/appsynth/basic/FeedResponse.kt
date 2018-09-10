213f36b0fecc8113d3da5641ef7fda724109274a
Jeeraphan <jeeraphan.lairat@gmail.com>
2018-09-10 12:30:33 +0700

    create response model

---
+++ app/src/main/java/net/appsynth/basic/FeedResponse.kt
@@ -0,0 +1,22 @@
+package net.appsynth.basic
+
+class FeedResponse {
+    var status: String? = null
+    val totalResults: Int? = null
+    var articles: ArrayList<Article>? = null
+}
+
+class Article {
+    val source: Source? = null
+    var author: String? = null
+    var title: String? = null
+    var description: String? = null
+    var url: String? = null
+    var urlToImage: String? = null
+    var publishedAt: String? = null
+}
+
+class Source {
+    var id: String? = null
+    var name: String? = null
+}
