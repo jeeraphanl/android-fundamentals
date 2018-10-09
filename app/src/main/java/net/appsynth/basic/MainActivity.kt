05c321e947b4f985b9c4a54fbd94b283e03ed64c
Jeeraphan Lairat <m@appsynth.net>
2018-10-09 21:55:56 +0700

    add selector file

--- app/src/main/java/net/appsynth/basic/MainActivity.kt
+++ app/src/main/java/net/appsynth/basic/MainActivity.kt
@@ -2,11 +2,16 @@ package net.appsynth.basic
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_calculator.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calculator)
+
+        sevenButton.setOnClickListener {
+            resultTextView.text = "7"
+        }
     }
 }
