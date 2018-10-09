79215760750fe9865e20ca2d136f7f7451b02044
Jeeraphan Lairat <m@appsynth.net>
2018-10-09 22:13:14 +0700

    add cal lifecycle

---
+++ app/src/main/java/net/appsynth/basic/CalculatorActivity.kt
@@ -0,0 +1,61 @@
+package net.appsynth.basic
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_calculator.*
+
+class CalculatorActivity : AppCompatActivity() {
+
+    companion object {
+        private const val TAG = "android-fundamentals"
+        private const val KEY_RESULT = "key_result"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d(TAG, "onCreate")
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calculator)
+
+        sevenButton.setOnClickListener {
+            resultTextView.text = "7"
+        }
+    }
+
+    override fun onStart() {
+        Log.d(TAG, "onStart")
+        super.onStart()
+    }
+
+    override fun onResume() {
+        Log.d(TAG, "onResume")
+        super.onResume()
+    }
+
+    override fun onPause() {
+        Log.d(TAG, "onPause")
+        super.onPause()
+    }
+
+    override fun onStop() {
+        Log.d(TAG, "onStop")
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        Log.d(TAG, "onDestroy")
+        super.onDestroy()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        outState?.putString(KEY_RESULT, resultTextView.text.toString())
+
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+        super.onRestoreInstanceState(savedInstanceState)
+        val result = savedInstanceState?.getString(KEY_RESULT)
+        resultTextView.text = result
+    }
+}
